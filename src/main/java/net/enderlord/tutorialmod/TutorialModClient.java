4d136d8780b6924368d19cd7d9e30e275478f202
Ethan Ledford <ethanledford@Ethans-Air.home>
2024-09-07 15:02:38 -0400

    Initial commit

---
+++ src/main/java/net/enderlord/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.enderlord.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
