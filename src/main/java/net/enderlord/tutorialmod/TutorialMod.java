0c09d2a346d71e8629a793ef2eaf7dcb99f8f547
EnderLord02 <ethan.ledford@hotmail.com>
2024-09-07 23:39:07 -0400

    Periodic Save

--- src/main/java/net/enderlord/tutorialmod/TutorialMod.java
+++ src/main/java/net/enderlord/tutorialmod/TutorialMod.java
@@ -1,5 +1,7 @@
 package net.enderlord.tutorialmod;
 
+import net.enderlord.tutorialmod.block.ModBlocks;
+import net.enderlord.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +13,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 
 	}
 }
---
+++ src/main/java/net/enderlord/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,26 @@
+package net.enderlord.tutorialmod.block;
+
+import net.enderlord.tutorialmod.TutorialMod;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    //4:02 #3 Kaupenjoe
+
+    private  static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return  Registry.register(Registries.BLOCK, Identifier.of(TutorialMod.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
+    }
+}
