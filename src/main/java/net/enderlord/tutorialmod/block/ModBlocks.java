4c0efe4f4147c7e2ac469df7e7b1e770d4ead1d4
EnderLord02 <ethan.ledford@hotmail.com>
2024-09-08 14:25:26 -0400

    Periodic Save

--- src/main/java/net/enderlord/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/enderlord/tutorialmod/block/ModBlocks.java
@@ -1,17 +1,24 @@
 package net.enderlord.tutorialmod.block;
 
 import net.enderlord.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-    //4:02 #3 Kaupenjoe
+    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
+    new Block(AbstractBlock.Settings.create().strength(4f)
+            .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
-    private  static Block registerBlock(String name, Block block) {
+    private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return  Registry.register(Registries.BLOCK, Identifier.of(TutorialMod.MOD_ID, name), block);
     }
@@ -22,5 +29,9 @@ public class ModBlocks {
     }
     public static void registerModBlocks() {
         TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(ModBlocks.PINK_GARNET_BLOCK);
+        });
     }
 }
